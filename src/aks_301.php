fd425c9b8d9be6810ab811bacf2a584927470644
makss <makss@users.noreply.github.com>
2025-05-02 16:52:16 +0300

    psr2

--- src/aks_301.php
+++ src/aks_301.php
@@ -1,36 +1,48 @@
 <?php
-function aks_301($atts) {
+
+function aks_301($atts)
+{
     extract(lAtts(array(
         'start'=>'',
         'url'=>'http://'.$_SERVER["SERVER_NAME"].'/',
         'ignore'=>'',
         'black'=>'cltreq\.asp|owssvr\.dll'
-    ),$atts));
+    ), $atts));
  
-    if( $ignore ){
-        $ignor=explode('|',$ignore);
-        for( $i = 0; $i < count($ignor); $i++ ){
-            if( preg_match("|".$ignor[$i]."|i", $_SERVER["REQUEST_URI"]) ){ return; }
+    if ($ignore) {
+        $ignor=explode('|', $ignore);
+        for ($i = 0; $i < count($ignor); $i++) {
+            if (preg_match("|".$ignor[$i]."|i", $_SERVER["REQUEST_URI"])) {
+                return;
+            }
         }
     }
  
-    if( $black ){
-        $bl=explode('|',$black);
-        for( $i = 0; $i < count($bl); $i++ ){
-            if( preg_match("|".$bl[$i]."|i", $_SERVER["REQUEST_URI"]) ){ header("HTTP/1.0 404 Not Found"); exit; }
+    if ($black) {
+        $bl=explode('|', $black);
+        for ($i = 0; $i < count($bl); $i++) {
+            if (preg_match("|".$bl[$i]."|i", $_SERVER["REQUEST_URI"])) {
+                header("HTTP/1.0 404 Not Found");
+                exit;
+            }
         }
     }
 
-    if( $start ){
-        $st=explode('|',$start); $ur=explode('|',$url);
-        for( $i = 0; $i < count($st); $i++ ){
-            if( preg_match("|^".$st[$i]."|i", $_SERVER["REQUEST_URI"]) ){
-                if( !$ur[$i] ){ $ur[$i] = $ur[0]; }
-                if( preg_match('/\(/', $st[$i]) ){         // check if regexp
+    if ($start) {
+        $st=explode('|', $start);
+        $ur=explode('|', $url);
+        for ($i = 0; $i < count($st); $i++) {
+            if (preg_match("|^".$st[$i]."|i", $_SERVER["REQUEST_URI"])) {
+                if (!$ur[$i]) {
+                    $ur[$i] = $ur[0];
+                }
+                // check if regexp
+                if (preg_match('/\(/', $st[$i])) {
                     $ur[$i] = preg_replace("|^".$st[$i]."|i", $ur[$i], $_SERVER["REQUEST_URI"]);
                 }
                 header('HTTP/1.1 301 Moved Permanently');
-                header("Location: ".$ur[$i]); exit;
+                header("Location: ".$ur[$i]);
+                exit;
             }
         }
     }
--- src/index.php
+++ src/index.php
@@ -1,7 +1,9 @@
 <?php
 
-if(@txpinterface == 'public'){
-    if(!in_array('aks_header_callback', ob_list_handlers() )){ ob_start('aks_header_callback'); }
+if (@txpinterface == 'public') {
+    if (!in_array('aks_header_callback', ob_list_handlers())) {
+        ob_start('aks_header_callback');
+    }
     if (class_exists('\Textpattern\Tag\Registry')) {
         Txp::get('\Textpattern\Tag\Registry')
             ->register('aks_301')
@@ -19,7 +21,8 @@ if(@txpinterface == 'public'){
         trigger_error("Unable to include required plugin 'aks_header'", E_USER_ERROR);  // E_USER_WARNING
     }
 */
-function aks_filter_add($name, $priority=50){
+function aks_filter_add($name, $priority = 50)
+{
     global $aks_filter;
     $aks_filter["{$priority}_{$name}"] = $name;
 }
